66a971eaf760e3a7aeaf3a33544920909d95557b
qwerttyuiiop1 <sherwynn_angeles@dlsu.edu.ph>
2024-09-19 00:22:48 +0800

    Fix build errors

--- app/src/main/java/com/example/hsrrelicmanager/core/exe/Instance.kt
+++ app/src/main/java/com/example/hsrrelicmanager/core/exe/Instance.kt
@@ -3,32 +3,11 @@ package com.example.hsrrelicmanager.core.exe
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
-abstract class Instance<T>: TaskScope<T>() {
-    protected abstract suspend fun run(): MyResult<T>
-    override fun start(scope: CoroutineScope) {
-        super.start(scope)
-        launch {
-            try {
-                val res = run()
-                exit(res)
-            } finally {
-                onClose()
-                close()
-            }
-        }
-    }
-    protected suspend fun exit(msg: MyResult<T>): Nothing = complete(msg)
-    protected open suspend fun onClose() {}
-}
-
-//abstract class Instance<T> private constructor(
-//    protected val scope: TaskScope<T>,
-//): TaskInstance<T> by scope {
-//    constructor(): this(TaskScope())
+//abstract class Instance<T>: TaskScope<T>() {
 //    protected abstract suspend fun run(): MyResult<T>
 //    override fun start(scope: CoroutineScope) {
-//        this.scope.start(scope)
-//        this.scope.launch {
+//        super.start(scope)
+//        launch {
 //            try {
 //                val res = run()
 //                exit(res)
@@ -38,9 +17,30 @@ abstract class Instance<T>: TaskScope<T>() {
 //            }
 //        }
 //    }
-//    protected val tick get() = scope.tick
-//    protected suspend fun <U> join(r: TaskInstance<U>) = scope.join(r)
-//    protected suspend fun awaitTick() = scope.awaitTick()
-//    protected suspend fun exit(msg: MyResult<T>): Nothing = scope.complete(msg)
+//    protected suspend fun exit(msg: MyResult<T>): Nothing = complete(msg)
 //    protected open suspend fun onClose() {}
 //}
+
+abstract class Instance<T> private constructor(
+    protected val scope: TaskScope<T>,
+): TaskInstance<T> by scope {
+    constructor(): this(TaskScope())
+    protected abstract suspend fun run(): MyResult<T>
+    override fun start(scope: CoroutineScope) {
+        this.scope.start(scope)
+        this.scope.launch {
+            try {
+                val res = run()
+                exit(res)
+            } finally {
+                onClose()
+                close()
+            }
+        }
+    }
+    protected val tick get() = scope.tick
+    protected suspend fun <U> join(r: TaskInstance<U>) = scope.join(r)
+    protected suspend fun awaitTick() = scope.awaitTick()
+    protected suspend fun exit(msg: MyResult<T>): Nothing = scope.complete(msg)
+    protected open suspend fun onClose() {}
+}
