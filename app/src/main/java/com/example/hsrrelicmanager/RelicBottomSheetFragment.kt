a34b39159347df6a26eeecfcf06289f60875d6ce
qwerttyuiiop1 <sherwynn_angeles@dlsu.edu.ph>
2024-10-19 02:04:49 +0800

    implement relic bottom sheet layout

--- app/src/main/java/com/example/hsrrelicmanager/RelicBottomSheetFragment.kt
+++ app/src/main/java/com/example/hsrrelicmanager/RelicBottomSheetFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class RelicBottomSheetFragment : BottomSheetDialogFragment() {
@@ -11,6 +12,23 @@ class RelicBottomSheetFragment : BottomSheetDialogFragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.relic_bottom_sheet, container, false)
+//        setStyle(STYLE_NORMAL, R.style.RelicBottomSheetDialogTheme);
+        val view = inflater.inflate(R.layout.relic_bottom_sheet, container, false)
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        dialog?.apply {
+            window?.clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND)
+            setOnShowListener {
+                val bottomSheet = findViewById<View>(com.google.android.material.R.id.design_bottom_sheet)
+                bottomSheet?.setBackgroundResource(android.R.color.transparent)
+//                val blurRadius = 30f
+//                val renderEffect = RenderEffect.createBlurEffect(blurRadius, blurRadius, Shader.TileMode.CLAMP)
+//                bottomSheet.setRenderEffect(renderEffect)
+
+            }
+        }
     }
 }
