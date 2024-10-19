ee79c54b58fe8f5e3950cc95f19d299d146c7425
qwerttyuiiop1 <sherwynn_angeles@dlsu.edu.ph>
2024-10-19 19:19:43 +0800

    Implement Select Filter Modal

--- app/src/main/java/com/example/hsrrelicmanager/AddActionGroupBodyFragment.kt
+++ app/src/main/java/com/example/hsrrelicmanager/AddActionGroupBodyFragment.kt
@@ -5,13 +5,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.hsrrelicmanager.databinding.FragmentActionGroupBodyBinding
 
 class AddActionGroupBodyFragment : Fragment() {
+    lateinit var binding: FragmentActionGroupBodyBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_action_group_body, container, false)
+    ): View {
+        return FragmentActionGroupBodyBinding.inflate(inflater, container, false).apply {
+            binding = this
+            binding.filterSectonAdd.setOnClickListener{
+                requireActivity().findViewById<View>(R.id.activity_main_layout).blur()
+                val dialog = AddFilterDialog()
+                dialog.show(parentFragmentManager, "AddFilterDialog")
+            }
+        }.root
     }
 
 }
--- app/src/main/java/com/example/hsrrelicmanager/RuleBodyFragment.kt
+++ app/src/main/java/com/example/hsrrelicmanager/RuleBodyFragment.kt
@@ -8,7 +8,6 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.hsrrelicmanager.GroupAdapter.ViewHolder
 import com.example.hsrrelicmanager.model.relics.Relic
 import com.example.hsrrelicmanager.model.rules.Filter
 import com.example.hsrrelicmanager.model.rules.action.EnhanceAction
