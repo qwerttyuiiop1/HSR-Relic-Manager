7e34f81e1c2f080b28dc04614fb87aea48a1f0ed
howard-rose <philip.anthon@gmail.com>
2024-11-26 11:18:44 +0800

    feat: delete rule dialog for categorized rules view
    TODO: deduplicate code

--- app/src/main/java/com/example/hsrrelicmanager/ui/RuleBodyFragment.kt
+++ app/src/main/java/com/example/hsrrelicmanager/ui/RuleBodyFragment.kt
@@ -17,7 +17,10 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.hsrrelicmanager.R
 import com.example.hsrrelicmanager.model.rules.group.ActionGroup
+import com.example.hsrrelicmanager.ui.CategorizedGroupAdapter.GroupViewHolder
 import java.util.Collections
+
+
 
 class RuleBodyFragment : Fragment() {
     override fun onCreateView(
@@ -174,6 +177,168 @@ class RuleBodyFragment : Fragment() {
         requireActivity().findViewById<View>(R.id.activity_main_layout).blur()
     }
 
+
+    private fun showDeleteRuleDialog(index: Int, group: ActionGroup) {
+        val dialog = DeleteRuleDialogFragment.newInstance(index, group)
+        dialog.show(parentFragmentManager, "DeleteRuleDialog")
+    }
+}
+
+class CategorizedRuleBodyFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.rule_body_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val groupData = (activity as MainActivity).groupData
+        val categorizedGroupAdapter = CategorizedGroupAdapter(groupData)
+
+        val recyclerView: RecyclerView = view.findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        recyclerView.adapter = categorizedGroupAdapter
+
+        var swipedItemIndex: Int = -1
+
+        // Listens for action from delete dialog
+        parentFragmentManager.setFragmentResultListener("delete_rule_request", viewLifecycleOwner) { _, bundle ->
+            val index = bundle.getInt("index")
+            val action = bundle.getString("action")
+
+            // Delete confirmed
+            if (action == "confirm" && index >= 0) {
+                val itemIndex = index
+                val groupIndex = (categorizedGroupAdapter.items.get(itemIndex) as CategorizedGroupAdapter.GroupItem).group.position;
+
+                categorizedGroupAdapter.items.removeAt(itemIndex)
+                categorizedGroupAdapter.groupData.removeAt(groupIndex)
+
+                for (i in groupIndex..<categorizedGroupAdapter.groupData.size) {
+                    groupData.get(i).position = i
+                }
+
+                categorizedGroupAdapter.notifyDataSetChanged()
+
+                Toast.makeText(requireContext(), "Rule trashed.", Toast.LENGTH_SHORT).show()
+
+                // Delete cancelled
+            } else if (action == "cancel" && swipedItemIndex != -1) {
+                categorizedGroupAdapter.notifyItemChanged(swipedItemIndex)
+            }
+            swipedItemIndex = -1
+        }
+
+        val itemTouchHelper = ItemTouchHelper(object : ItemTouchHelper.Callback() {
+            override fun getMovementFlags(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder
+            ): Int {
+                if (viewHolder !is GroupViewHolder)
+                    return makeMovementFlags(0, 0)
+
+                return makeMovementFlags(
+                    0,
+                    ItemTouchHelper.LEFT
+                )
+            }
+
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false;
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val itemIndex = viewHolder.adapterPosition
+                val groupIndex = (categorizedGroupAdapter.items.get(itemIndex) as CategorizedGroupAdapter.GroupItem).group.position
+
+                val group = categorizedGroupAdapter.groupData[groupIndex]
+
+                swipedItemIndex = itemIndex
+
+                blurBackground()
+                showDeleteRuleDialog(itemIndex, group)
+            }
+
+            override fun onChildDraw(
+                c: Canvas,
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                dX: Float,
+                dY: Float,
+                actionState: Int,
+                isCurrentlyActive: Boolean
+            ) {
+                val itemView = viewHolder.itemView
+                val card: LinearLayout = itemView.findViewById(R.id.group_card)
+                val backgroundDrawable = card.background as GradientDrawable
+
+                val trash: ImageView = itemView.findViewById(R.id.trash_icon)
+                val upArrow: ImageView = itemView.findViewById(R.id.ivUpArrow)
+                val downArrow: ImageView = itemView.findViewById(R.id.ivDownArrow)
+                val position: TextView = itemView.findViewById(R.id.tvPosition)
+
+                // Swiping horizontally
+                if (Math.abs(dX) > Math.abs(dY)) {
+                    val swipeRatio = Math.abs(dX) / itemView.width * 1.6
+                    val alphaSwipeRatio = (swipeRatio * 2).coerceIn(0.0, 1.0).toFloat()
+
+                    val startColor = arrayOf(255, 255, 255)
+                    val endColor = arrayOf(219, 88, 86)
+
+                    val red = (startColor[0] + (endColor[0] - startColor[0]) * swipeRatio).toInt()
+                    val green = (startColor[1] + (endColor[1] - startColor[1]) * swipeRatio).toInt()
+                    val blue = (startColor[2] + (endColor[2] - startColor[2]) * swipeRatio).toInt()
+
+                    val strokeColor = Color.rgb(red, green, blue)
+                    backgroundDrawable.setStroke(3, strokeColor)
+
+                    trash.alpha = alphaSwipeRatio
+                    trash.visibility = View.VISIBLE
+
+                    // Swipe vertically
+                } else if (Math.abs(dX) < Math.abs(dY)) {
+                    card.setBackgroundResource(R.drawable.bg_inventory_relic_item_selected)
+
+                    upArrow.setColorFilter(Color.parseColor("#FFC65C"));
+                    downArrow.setColorFilter(Color.parseColor("#FFC65C"));
+                    upArrow.alpha = 1f
+                    downArrow.alpha = 1f
+
+                    position.setTextColor(Color.parseColor("#FFC65C"));
+                    position.alpha = 1f
+
+                    // Stationary
+                } else {
+                    card.setBackgroundResource(R.drawable.bg_dark)
+                    trash.visibility = View.GONE
+
+                    upArrow.setColorFilter(Color.parseColor("#FFFFFF"));
+                    downArrow.setColorFilter(Color.parseColor("#FFFFFF"));
+                    upArrow.alpha = 0.5f
+                    downArrow.alpha = 0.5f
+
+                    position.setTextColor(Color.parseColor("#FFFFFF"));
+                    position.alpha = 0.5f
+                }
+
+                itemView.translationX = dX
+
+                super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+            }
+        })
+        itemTouchHelper.attachToRecyclerView(recyclerView)
+    }
+
+    private fun blurBackground() {
+        requireActivity().findViewById<View>(R.id.activity_main_layout).blur()
+    }
 
     private fun showDeleteRuleDialog(index: Int, group: ActionGroup) {
         val dialog = DeleteRuleDialogFragment.newInstance(index, group)
